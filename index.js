14e17d7fdfaa0520199161bbfaac3b5f97a92773
Shuhei Kagawa <s-kagawa@m3.com>
2014-10-30 21:16:04 +0900

    Initial commit. 0.0.1.

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var to5 = require('6to5');
+
+function createPreprocessor(args, config, logger, helper) {
+  var log = logger.create('preprocessor.6to5');
+
+  function sixToFive(content, file, basePath, done) {
+    log.debug('Processing "%s".', file.originalPath);
+    file.path = file.originalPath + '-compiled.js';
+
+    var processed = null;
+    try {
+      processed = to5.transform(content, {
+        filename: file.originalPath
+      }).code;
+    } catch (e) {
+      log.debug('%s\n at %s', e.message, file.originalPath);
+    }
+
+    done(processed);
+  }
+
+  return sixToFive;
+}
+
+createPreprocessor.$inject =
+  ['args', 'config.6to5Preprocessor', 'logger', 'helper'];
+
+module.exports = {
+  'preprocessor:6to5': ['factory', createPreprocessor]
+};
