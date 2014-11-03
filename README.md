1612fe52cf8c861951d98d52da681a455d089507
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2014-11-03 11:55:40 +0900

    Pass options to 6to5.

--- README.md
+++ README.md
@@ -10,4 +10,40 @@ npm install karma-6to5-preprocessor --save-dev
 
 ## Configuration
 
-Not implemented yet but coming soon...
+See [6to5 options](https://github.com/sebmck/6to5#options) for more details.
+
+Given `options` properties are passed to 6to5 with no change except:
+
+- filename
+- sourceMapName
+- sourceFileName
+
+Because they should differ from file to file, corresponding configuration functions are available.
+
+For example, inline sourcemap configuration would look like the following.
+
+```js
+module.exports = function(config) {
+  config.set({
+    files: [
+      'src/**/*.js',
+      'test/**/*.js'
+    ],
+    preprocessors: {
+      'src/**/*.js': ['6to5'],
+      'test/**/*.js': ['6to5']
+    },
+    '6to5Preprocessor': {
+      options: {
+        sourceMap: 'inline'
+      },
+      filename: function(file) {
+        return file.originalPath.replace(/\.js$/, '.es5.js');
+      },
+      sourceFileName: function(file) {
+        return file.originalPath;
+      }
+    }
+  });
+};
+```
--- index.js
+++ index.js
@@ -1,23 +1,41 @@
-'use strict';
+/* jshint node:true */
 
 var to5 = require('6to5');
+var extend = require('util')._extend;
+
+var PER_FILE_OPTIONS = [
+  'filename',
+  'sourceMapName',
+  'sourceFileName'
+];
 
 function createPreprocessor(args, config, logger, helper) {
   var log = logger.create('preprocessor.6to5');
 
   function sixToFive(content, file, done) {
     log.debug('Processing "%s".', file.originalPath);
-    file.path = file.originalPath + '-compiled.js';
+    var options = createOptions(config, file);
+    file.path = options.filename || file.path;
 
     try {
-      var processed = to5.transform(content, {
-        filename: file.originalPath
-      }).code;
+      var processed = to5.transform(content, options).code;
       done(null, processed);
     } catch (e) {
       log.error('%s\n at %s', e.message, file.originalPath);
       done(e, null);
     }
+  }
+
+  function createOptions(config, file) {
+    config = config || {};
+    var options = extend({ filename: file.originalPath }, config.options || {});
+    PER_FILE_OPTIONS.forEach(function(optionName) {
+      var configFunc = config[optionName];
+      if (typeof configFunc === 'function') {
+        options[optionName] = configFunc(file);
+      }
+    });
+    return options;
   }
 
   return sixToFive;
